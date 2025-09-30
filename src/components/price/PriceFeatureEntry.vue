be7b1fb9b2014fe212eb0cacb7dd3975e250da47
Kikyeong Kwon (Kate) <143983417+kiiiiiki@users.noreply.github.com>
2025-10-01 04:02:18 +0900

    Create PriceFeatureEntry.vue
    feat(price): add PriceFeatureEntry

---
+++ src/components/price/PriceFeatureEntry.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="space-y-6 p-4">
+    <PriceTrendCharts />
+    <BestDealSticky />
+    <ReportsFAB />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { usePriceStore } from '@/stores/priceStore'
+import PriceTrendCharts from './PriceTrendCharts.vue'
+import BestDealSticky from './BestDealSticky.vue'
+import ReportsFAB from './ReportsFAB.vue'
+
+const store = usePriceStore()
+onMounted(()=> store.bootstrap())
+</script>
